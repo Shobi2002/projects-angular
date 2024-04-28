094192a31cabf1ff509db44865e7bc6a60607459
Shobi2002 <133584396+Shobi2002@users.noreply.github.com>
2024-04-28 12:31:07 +0530

    changes made
    changes made

---
+++ Shopping/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Shopping';
+  item_count=0;
+  addtoCart(){
+    this.item_count++;
+  }  
+  removeFromCart(){
+    if(this.item_count>0){
+     this.item_count--;
+    }
+  
+ }
+}
